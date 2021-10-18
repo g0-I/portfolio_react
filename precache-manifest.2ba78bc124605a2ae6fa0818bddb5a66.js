18abf5a82ea5321c79a239d29a7f577bc56db4f1
0k1ta <freeer55@gmail.com>
2021-10-18 16:00:55 +0900

    Updates

---
+++ precache-manifest.2ba78bc124605a2ae6fa0818bddb5a66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7de324b747c951877f8eca9cfc2260",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "9422d95cadcc03f43ee4",
+    "url": "/portfolio_react/static/css/main.9567db27.chunk.css"
+  },
+  {
+    "revision": "c18a637786f7dc0c4bee",
+    "url": "/portfolio_react/static/js/2.2b01b886.chunk.js"
+  },
+  {
+    "revision": "9422d95cadcc03f43ee4",
+    "url": "/portfolio_react/static/js/main.18d227d0.chunk.js"
+  },
+  {
+    "revision": "c91455f75e20bb8f5bf7",
+    "url": "/portfolio_react/static/js/runtime~main.269d08db.js"
+  },
+  {
+    "revision": "1b64e6d5883509d808b48df1f61908a5",
+    "url": "/portfolio_react/static/media/github.1b64e6d5.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/portfolio_react/static/media/html.664a8c00.png"
+  },
+  {
+    "revision": "0aad6ddc383c0d910871861c842796bb",
+    "url": "/portfolio_react/static/media/portfolio.0aad6ddc.png"
+  },
+  {
+    "revision": "29f8051baa576ae10d62b8c685fbe34a",
+    "url": "/portfolio_react/static/media/python.29f8051b.png"
+  }
+]);
